26da6ab624041053f3abbd176cf3900471f0d6c1
Rajat Ghorai <Rajat.Ghorai@altisource.com>
2015-08-19 01:05:20 +0530

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pentadotsApp
+ * @description
+ * # pentadotsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pentadotsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pentadotsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pentadotsApp
+ */
+angular.module('pentadotsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
